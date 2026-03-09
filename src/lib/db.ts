1641ae57e608cdddc4807dac3edbf1cc9d05bdba
Pablo Alberto Gonzalez <pgonzalez@vialidad.gob.ar>
2026-03-09 16:10:45 -0300

    Fix: Set ssl: false on MariaDB connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,7 +8,8 @@ const pool = mysql.createPool({
     port: parseInt(process.env.DB_PORT || '3306'),
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: false
 });
 
 export default pool;
