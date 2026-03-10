d46a69610c1b67901014737c5773afde799f82cf
Pablo Alberto Gonzalez <pgonzalez@vialidad.gob.ar>
2026-03-10 09:44:37 -0300

    Update migrate-v2.js

--- migrate-v2.js
+++ migrate-v2.js
@@ -18,10 +18,7 @@ async function migrate() {
         user: config.DB_USER,
         password: config.DB_PASSWORD,
         database: config.DB_NAME,
-        port: Number(config.DB_PORT) || 3306,
-        ssl: {
-            rejectUnauthorized: false
-        }
+        port: Number(config.DB_PORT) || 3306
     });
 
     try {
